810122b10fbe5b649d63cef93667bc4cd5381c4d
JmcaDev <jmcavzla@gmail.com>
2024-11-25 20:48:23 -0400

    Restore password

--- src/views/auth/LoginView.tsx
+++ src/views/auth/LoginView.tsx
@@ -30,9 +30,14 @@ export default function LoginView() {
 
   return (
     <>
+      <h1 className="text-5xl font-black text-white">Iniciar Sesion</h1>
+      <p className="text-2xl font-light text-white mt-5">
+          Comienza a planear tus proyectos {''}
+          <span className=" text-fuchsia-500 font-bold"> iniciando sesion en este formulario</span>
+      </p>
       <form
         onSubmit={handleSubmit(handleLogin)}
-        className="space-y-8 p-10 bg-white"
+        className="space-y-8 p-10 bg-white mt-10"
         noValidate
       >
         <div className="flex flex-col gap-5">
@@ -88,6 +93,11 @@ export default function LoginView() {
           to={"/auth/register"}
           className="text-center text-gray-300 font-normal"
         >¿No tienes cuenta? Crear una</Link>
+
+        <Link
+          to={"/auth/forgot-password"}
+          className="text-center text-gray-300 font-normal"
+        >¿Olvidates tu password? Reestablecer</Link>
       </nav>
     </>
   )
